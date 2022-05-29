3aa4e70f2b28051dfa13bc0dfdbdf95fab6794e5
mukhsinafl <80018847+mukhsinafl@users.noreply.github.com>
2022-05-29 16:21:16 +0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -15,8 +15,53 @@ cout<<"1: Addition        \t \t \t7: Sin "<<endl
     <<"6: Square Root     \t \t \t12: Inverse Tan "<<endl;
 }
 
-int main() {
+class op_dasar {
+    public:
+    int a;
+    float b,x,y,hasil;
+    void pilihan();
+    void substract(int a,int b);
+    void exponent(int a,int b);
+    void tangen(int a,int b);};
+    
+void op_dasar::substract(int a,int b){
+    hasil= a+b;
+    cout<<"hasilnya adalah "<<hasil<<endl;
+    
+}
+void op_dasar::exponent(int a,int b){
+    hasil= pow(a,b);
+    cout<<"hasilnya adalah "<<hasil<<endl;
+}
+
+void op_dasar::pilihan() {
+    cout << "Enter your choice" << endl;
+    cin >> a;
+    switch(a){
+    case 2:
+        cout << "Enter first number" << endl;
+        cin >> x;
+        cout << "Enter second number" << endl;
+        cin >> y;
+        substract(x,y);
+        break;
+    case 5:
+        cout << "Enter first number" << endl;
+        cin >> x;
+        cout << "Enter second number" << endl;
+        cin >> y;
+        exponent(x,y);
+        break;
+    default:
+        cout << "Invalid input. Try Again!" << endl;
+    }
+}
+
+int main(){
+    op_dasar Calculator;
     menu();
+    Calculator.pilihan();
+    
   return 0;
 }
 
