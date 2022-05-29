2bd7e903e9c35d6239b6649cb6d2ed147df3f8ca
favrng <fiorellaaverinag@gmail.com>
2022-05-29 16:41:37 +0700

    test return

--- Main.cpp
+++ Main.cpp
@@ -20,38 +20,27 @@ class op_dasar {
     int a;
     float b,x,y,hasil;
     void pilihan();
+    int addition (int a, int b);
     void substract(int a,int b);
     void exponent(int a,int b);
     void tangen(int a,int b);};
     
-void op_dasar::substract(int a,int b){
-    hasil= a+b;
-    cout<<"hasilnya adalah "<<hasil<<endl;
-    
+int op_dasar::addition(int a, int b){
+    hasil = a+b;
+    return hasil;m
 }
-void op_dasar::exponent(int a,int b){
-    hasil= pow(a,b);
-    cout<<"hasilnya adalah "<<hasil<<endl;
-}
+
 
 void op_dasar::pilihan() {
     cout << "Enter your choice" << endl;
     cin >> a;
     switch(a){
-    case 2:
+        case 1:
         cout << "Enter first number" << endl;
         cin >> x;
         cout << "Enter second number" << endl;
         cin >> y;
-        substract(x,y);
-        break;
-    case 5:
-        cout << "Enter first number" << endl;
-        cin >> x;
-        cout << "Enter second number" << endl;
-        cin >> y;
-        exponent(x,y);
-        break;
+        addition (x,y);
     default:
         cout << "Invalid input. Try Again!" << endl;
     }
